9437fc6c6a47422c95fe21df1ee640bbfd6e1db7
JeetDas5 <jeet15083011@gmail.com>
2026-05-01 23:17:39 +0530

    feat: implement zod validations

--- src/actions/auth.actions.ts
+++ src/actions/auth.actions.ts
@@ -1,27 +1,29 @@
 "use server";
 
+import z from "zod";
+import { cookies } from "next/headers";
+
 import {
   initiateSignup,
   verifyEmail as verifyEmailService,
   login,
 } from "@/lib/auth-service";
 import { signToken } from "@/lib/jwt";
-import { cookies } from "next/headers";
+import { loginSchema, registerSchema } from "@/validations/auth.validations";
 
 export async function signUpAction(formData: FormData) {
   const name = formData.get("name") as string;
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
-  if (!name || !email || !password) {
-    return { error: "Missing required fields" };
-  }
-
   try {
+    registerSchema.parse({ name, email, password });
     const result = await initiateSignup({ name, email, password });
     return { success: true, message: result.message };
   } catch (error) {
-    if (error instanceof Error) {
+    if (error instanceof z.ZodError) {
+      return { error: error.issues[0].message };
+    } else if (error instanceof Error) {
       return { error: error.message || "Signup failed" };
     }
     return { error: "Signup failed" };
@@ -44,13 +46,10 @@ export async function loginAction(formData: FormData) {
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
-  if (!email || !password) {
-    return { error: "Missing required fields" };
-  }
-
   try {
+    loginSchema.parse({ email, password });
     const result = await login({ email, password });
-    
+
     // Set cookie
     const token = signToken({ userId: result.user.id });
     (await cookies()).set("session", token, {
@@ -62,7 +61,11 @@ export async function loginAction(formData: FormData) {
 
     return { success: true };
   } catch (error) {
-    if (error instanceof Error) {
+    if (error instanceof z.ZodError) {
+      return { error: error.issues[0].message };
+    } else if (error instanceof z.ZodError) {
+      return { error: error.issues[0].message };
+    } else if (error instanceof Error) {
       return { error: error.message || "Login failed" };
     }
     return { error: "Login failed" };
